fda51b6c643d00a0029ab5cd436a2200350c04ed
Ran Leibovitz <leibovitzran@gmail.com>
2021-08-30 13:24:59 +0300

    valid

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,18 +3,25 @@ import React, { Component } from "react";
 export default class About extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      show: "none",
+    };
+    this.alert = this.alert.bind(this);
   }
 
   saveName(e) {
     if (e.target.value.match(/[a-z]{3,5}/gim)) {
       localStorage.setItem("name", e.target.value);
+      return true;
     }
+    return false;
   }
 
   navigate = () => {
     this.props.history.push("/contact");
   };
+
+  alert() {}
 
   render() {
     return (
@@ -24,9 +31,29 @@ export default class About extends Component {
           type="text"
           placeholder="Enter your name"
           className="name"
+          style={{
+            position: "relative",
+          }}
           onBlur={this.saveName}
         />
-        <button onClick={this.navigate}>navigate</button>
+        <button
+          onClick={() => {
+            if (this.saveName()) {
+              this.navigate();
+            } else {
+              this.setState({ show: "" });
+            }
+          }}
+        >
+          navigate
+        </button>
+        <div
+          style={{
+            display: this.state.show,
+          }}
+        >
+          Please write only letters not numbers
+        </div>
       </div>
     );
   }
--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -3,7 +3,10 @@ import React, { Component } from "react";
 export default class Contact extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      show: "none",
+      flag: false,
+    };
   }
 
   savePhone(e) {
